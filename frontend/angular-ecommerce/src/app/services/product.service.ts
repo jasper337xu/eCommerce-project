b40b3b0ce7191246613f03b609708c76acfee399
Jasper Xu <xzh1401759561@gmail.com>
2020-07-23 22:44:01 -0400

    migrate Product to /model

--- frontend/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { Product } from '../common/product';
+import { Product } from '../model/product';
 
 @Injectable({
   providedIn: 'root'
