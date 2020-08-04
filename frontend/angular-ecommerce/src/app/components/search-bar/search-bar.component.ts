ad2ba26349186eb071b18828e17efa9105163f82
Jasper Xu <xzh1401759561@gmail.com>
2020-08-03 22:29:56 -0400

    create search bar component to support keyword search

---
+++ frontend/angular-ecommerce/src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  doSearch(keyword: string) {
+    console.log(`input keyword is ${keyword}`);
+    this.router.navigateByUrl(`/search/${keyword}`);
+  }
+
+}
