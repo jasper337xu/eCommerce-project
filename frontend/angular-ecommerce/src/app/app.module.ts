284fbc8c6f09aebe8aaf5360ff5d11babb71d900
Jasper Xu <xzh1401759561@gmail.com>
2020-08-06 22:49:28 -0400

    Migrate routing to the new app-routing module

--- frontend/angular-ecommerce/src/app/app.module.ts
+++ frontend/angular-ecommerce/src/app/app.module.ts
@@ -1,42 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
-import { Routes, RouterModule } from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
-
-const routes: Routes = [
-  {
-    path: 'category/:id',
-    component: ProductListComponent,
-  },
-  {
-    path: 'category',
-    component: ProductListComponent,
-  },
-  {
-    path: 'products',
-    component: ProductListComponent,
-  },
-  {
-    path: 'search/:keyword',
-    component: ProductListComponent,
-  },
-  {
-    path: '',
-    redirectTo: '/products',
-    pathMatch: 'full',
-  },
-  {
-    path: '**',
-    redirectTo: '/products',
-    pathMatch: 'full',
-  }
-];
 
 @NgModule({
   declarations: [
@@ -46,9 +17,9 @@ const routes: Routes = [
     SearchBarComponent
   ],
   imports: [
-    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
