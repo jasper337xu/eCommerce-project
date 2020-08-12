2b931fe7933bad3b07e5310aae32712a814bcce2
Jasper Xu <xzh1401759561@gmail.com>
2020-08-11 23:02:16 -0400

    Import the new AppStoreModule for ngrx

--- frontend/angular-ecommerce/src/app/app.module.ts
+++ frontend/angular-ecommerce/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { AppStoreModule } from './app-store.module';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
@@ -22,6 +23,7 @@ import { ProductDetailsComponent } from './components/product-details/product-de
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    AppStoreModule,
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
