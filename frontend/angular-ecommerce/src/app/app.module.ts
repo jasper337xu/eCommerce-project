22466f8b7cee0c0b769ec982a2517226e04cfdf7
Jasper Xu <xzh1401759561@gmail.com>
2020-08-16 23:49:24 -0400

    Import NgbModule

--- frontend/angular-ecommerce/src/app/app.module.ts
+++ frontend/angular-ecommerce/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppStoreModule } from './app-store.module';
 
@@ -23,6 +24,7 @@ import { ProductDetailsComponent } from './components/product-details/product-de
   imports: [
     BrowserModule,
     HttpClientModule,
+    NgbModule,
     AppRoutingModule,
     AppStoreModule,
   ],
