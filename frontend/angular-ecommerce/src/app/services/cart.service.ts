21c32117dda900ef3812da375ddc7b971aabe12b
Jasper Xu <xzh1401759561@gmail.com>
2020-08-30 19:10:56 -0400

    Refactor the code of addToCart

--- frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -15,20 +15,15 @@ export class CartService {
 
   addToCart(cartItem: CartItem) {
     // check if we already have the item in our cart
-    let alreadyExistsInCart: boolean;
-    alreadyExistsInCart = this.cartItems.some(tempItem => tempItem.id === cartItem.id);
+    let existingCartItem: CartItem = undefined;
+    existingCartItem = this.cartItems.find(tempItem => tempItem.id === cartItem.id);
 
-    if (alreadyExistsInCart) {
-      // increment the quantity of the item
-      for (let tempItem of this.cartItems) {
-        if (tempItem.id === cartItem.id) {
-          tempItem.quantity++;
-          break;
-        }
-      }
+    if (existingCartItem != undefined) {
+      // the item already exists in the cart, increment the quantity of the item
+      existingCartItem.quantity++;
     }
     else {
-      // add the item to the cartItems
+      // the item does not exist in the cart, add the item to the cart
       this.cartItems.push(cartItem);
     }
 
